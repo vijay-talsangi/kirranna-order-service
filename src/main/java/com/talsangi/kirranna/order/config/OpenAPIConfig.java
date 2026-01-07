5c734c30be25ba623c39fd9d684871e45b8591ed
vijay-talsangi <vijaytalsangi4705@gmail.com>
2026-01-07 19:41:55 +0530

    Added API Documentation with Swagger and OpenAPI

---
+++ src/main/java/com/talsangi/kirranna/order/config/OpenAPIConfig.java
@@ -0,0 +1,20 @@
+package com.talsangi.kirranna.order.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAPIConfig {
+
+    @Bean
+    public OpenAPI orderServiceAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Order Service API")
+                        .description("This is the REST API for Order Service of Kirranna")
+                        .version("v0.0.1")
+                        .license(new License().name("Apache 2.0")));
+    }
+}
